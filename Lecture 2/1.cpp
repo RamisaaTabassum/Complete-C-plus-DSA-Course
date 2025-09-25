3334434ad87fef83eb076055b48b4738fba1cd50
RamisaaTabassum <ramisatabassum1234@gmail.com>
2025-09-25 08:40:48 +0600

    Add Lecture 2 files: 1.cpp and 2.cpp

---
+++ Lecture 2/1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<< "Ramisa"<<"\n"<<"Tabassum";
+ return 0;
+}
---
+++ Lecture 2/2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age = 23;
+char grade ='A';
+float PI = 3.14f;
+bool issafe = true;
+double price =100.99;
+cout<<sizeof(age)<<endl;
+cout<<grade<<endl;
+cout<< PI <<endl;
+cout<< issafe <<endl;
+cout<< price<<endl;
+}
